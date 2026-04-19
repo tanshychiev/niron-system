c03f6c9cd33c1995baf59d84e65d506d7eca2538
Tanshychiev <YOUR_EMAIL_HERE>
2026-04-19 14:41:13 +0700

    update

--- config/settings.py
+++ config/settings.py
@@ -76,7 +76,8 @@ TIME_ZONE = "UTC"
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
