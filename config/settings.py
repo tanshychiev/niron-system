c297f4720352d6f86768c14e53055f256faa9af5
Tanshychiev <YOUR_EMAIL_HERE>
2026-04-19 14:47:02 +0700

    fix static root and allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -4,7 +4,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = "django-insecure-change-this"
 DEBUG = True
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "localhost",
+    "niron-system.com",      # change if needed
+    "www.niron-system.com",  # change if needed
+    "your-server-ip",        # change if needed
+]
 
 INSTALLED_APPS = [
     "django.contrib.admin",
